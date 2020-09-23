42c157aa819146efd4bbcff0fce14441ef02fd38
Petr Sladek <slady@centrum.cz>
2020-09-23 14:55:00 +0200

    javadoc

--- src/test/java/net/slady/bsc/TestHelper.java
+++ src/test/java/net/slady/bsc/TestHelper.java
@@ -7,8 +7,16 @@ import org.junit.Assert;
  */
 public class TestHelper {
 
+	/**
+	 * The Delta for comparing double values.
+	 */
 	private static final double DELTA = 0.0001;
 
+	/**
+	 * The helper for calling <code>Assert.assertEquals()</code> with double values.
+	 * @param expected the expected value
+	 * @param actual the actual value
+	 */
 	public static void assertDoubleEquals(final double expected, final double actual) {
 		Assert.assertEquals(expected, actual, DELTA);
 	}
