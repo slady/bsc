32ebf2036dc3c3c73a448f44975a29ad87ca68c7
Petr Sladek <slady@centrum.cz>
2020-09-23 10:46:34 +0200

    the main input loop

--- src/main/java/net/slady/bsc/PackageDelivery.java
+++ src/main/java/net/slady/bsc/PackageDelivery.java
@@ -1,5 +1,11 @@
 package net.slady.bsc;
 
+import net.slady.bsc.entities.PostalCodeWeightPair;
+import net.slady.bsc.exceptions.IncorrectWeightException;
+import net.slady.bsc.exceptions.InputFormatException;
+import net.slady.bsc.service.InputParserService;
+import net.slady.bsc.service.PackageService;
+
 import java.util.Scanner;
 
 /**
@@ -10,15 +16,57 @@ import java.util.Scanner;
 public class PackageDelivery {
 
 	/**
+	 * The "quit" keyword to exit the program loop.
+	 */
+	public static final String KEYWORD_QUIT = "quit";
+
+	/**
 	 * The entry point of the Package Delivery program.
 	 * @param args command line arguments as described in the README file
 	 */
 	public static void main(final String... args) {
-		// create a Scanner using the InputStream available
+		final InputParserService inputParserService = new InputParserService();
+		final PackageService packageService = new PackageService();
+
+		// create a Scanner using the InputStream
 		final Scanner scanner = new Scanner(System.in);
-		// use the Scanner to read a line of text from the user
-		final String input = scanner.nextLine();
-		;
+
+		// infinite loop
+		while (true) {
+			// use the Scanner to read a line of text from the user
+			final String input = scanner.nextLine();
+			final String trim = input.trim();
+
+			// ignore empty lines
+			if (trim.length() == 0) {
+				continue;
+			}
+
+			// exit the infinite loop on the "quit" keyword
+			if (KEYWORD_QUIT.equalsIgnoreCase(trim.toLowerCase())) {
+				break;
+			}
+
+			final PostalCodeWeightPair postalCodeWeightPair;
+
+			try {
+				// parse the input text
+				postalCodeWeightPair = inputParserService.parsePostalCodeAndWeight(input);
+			} catch (final InputFormatException e) {
+				// handle InputFormatException
+				System.err.println("Error: Incorrect input format!");
+				System.err.println("Input line format:");
+				System.err.println("<weight: positive number, >0, maximal 3 decimal places, . (dot) as decimal separator><space><postal code: fixed 5 digits>");
+				continue;
+			} catch (final IncorrectWeightException e) {
+				// handle IncorrectWeightException
+				System.err.println("Error: Incorrect input weight!");
+				System.err.println("Weight must be a positive number!");
+				continue;
+			}
+
+			packageService.add(postalCodeWeightPair.getPostalCode(), postalCodeWeightPair.getWeight());
+		}
 	}
 
 }
