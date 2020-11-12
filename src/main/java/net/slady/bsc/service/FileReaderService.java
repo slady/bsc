0cc927bc0d8453759c5a6dbc249e7831cb777aac
Petr Sladek <slady@centrum.cz>
2020-11-12 09:47:09 +0100

    javadoc

--- src/main/java/net/slady/bsc/service/FileReaderService.java
+++ src/main/java/net/slady/bsc/service/FileReaderService.java
@@ -10,18 +10,37 @@ import java.io.FileReader;
 import java.io.IOException;
 
 /**
-*/
+ * Service for reading input files.
+ */
 public class FileReaderService {
 
+	/**
+	 * The InputParserService for reading values.
+	 */
 	private final InputParserService inputParserService;
 
+	/**
+	 * The PackageService for storing values.
+	 */
 	private final PackageService packageService;
 
+	/**
+	 * The constructor wit FileReaderService.
+	 * @param inputParserService the InputParserService for reading values
+	 * @param packageService the PackageService for storing values
+	 */
 	public FileReaderService(final InputParserService inputParserService, final PackageService packageService) {
 		this.inputParserService = inputParserService;
 		this.packageService = packageService;
 	}
 
+	/**
+	 * Imports weight files.
+	 * @param weightsFileName input file name of the file containing weights information
+	 * @throws IOException if I/O goes wrong
+	 * @throws IncorrectWeightException in the case of an incorrect weight
+	 * @throws InputFormatException in the case of an incorrect input format
+	 */
 	public void weightsImport(final String weightsFileName) throws IOException, IncorrectWeightException, InputFormatException {
 		try (BufferedReader br = new BufferedReader(new FileReader(weightsFileName))) {
 			String line;
@@ -32,6 +51,13 @@ public class FileReaderService {
 		}
 	}
 
+	/**
+	 * Imports prices files.
+	 * @param pricesFileName input file name of the file containing price information
+	 * @throws IOException in case I/O goes wrong
+	 * @throws InputFormatException in the case of an incorrect input format
+	 * @throws IncorrectWeightException in the case of an incorrect weight
+	 */
 	public void pricesImport(final String pricesFileName) throws IOException, InputFormatException, IncorrectWeightException {
 		try (BufferedReader br = new BufferedReader(new FileReader(pricesFileName))) {
 			String line;
