5be5d46790fb75384b62bb692b0d20715e81a05a
slady <slady@centrum.cz>
2020-11-12 09:26:44 +0100

    Update FileReaderService.java

--- src/main/java/net/slady/bsc/service/FileReaderService.java
+++ src/main/java/net/slady/bsc/service/FileReaderService.java
@@ -9,6 +9,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+*/
 public class FileReaderService {
 
 	private final InputParserService inputParserService;
